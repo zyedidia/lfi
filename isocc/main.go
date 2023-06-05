212cd935743f82ae9a5f74f5ddfcb5617b1a75cd
Zachary Yedidia <zyedidia@gmail.com>
2023-06-04 22:17:11 -0700

    Much better gcc wrapper

--- isocc/main.go
+++ isocc/main.go
@@ -8,9 +8,6 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
-
-	"github.com/kballard/go-shellquote"
-	"github.com/spf13/pflag"
 )
 
 var isoflags = []string{"-ffixed-x20", "-ffixed-x21", "-falign-labels=8", "-falign-jumps=8", "-falign-functions=8"}
@@ -31,8 +28,8 @@ func run(command string, args ...string) {
 	}
 }
 
-func temp() string {
-	tmp, err := os.CreateTemp("", "isocc.*.S")
+func temp(dir string) string {
+	tmp, err := os.CreateTemp(dir, ".isocc.*.S")
 	if err != nil {
 		fatal(err)
 	}
@@ -40,63 +37,53 @@ func temp() string {
 	return tmp.Name()
 }
 
-func addFlags(flags []string, prefix string, values *[]string) []string {
-	if values == nil {
-		return flags
-	}
-	for _, val := range *values {
-		flags = append(flags, prefix+val)
-	}
-	return flags
-}
-
 func main() {
-	xcc := pflag.String("Xcc", "", "extra c compiler flags")
-	defines := pflag.StringSliceP("define", "D", nil, "defines")
-	includes := pflag.StringSliceP("include", "I", nil, "includes")
-	opt := pflag.StringP("opt", "O", "0", "optimization level")
-	debug := pflag.BoolP("debug", "g", false, "include debugging information")
-	out := pflag.StringP("output", "o", "a.out", "output file")
-	compile := pflag.BoolP("emit-obj", "c", false, "compile but do not link")
-	assemble := pflag.BoolP("emit-asm", "S", false, "emit assembly code")
-	verbose := pflag.BoolP("verbose", "V", false, "verbose information")
-	keep := pflag.Bool("keep", false, "do not remove temporary files")
-	fflag := pflag.StringSliceP("f", "f", nil, "-f flags passed to gcc")
-	mflag := pflag.StringSliceP("m", "m", nil, "-m flags passed to gcc")
-	idirafterflag := pflag.StringSlice("idirafter", nil, "-idirafter flags passed to gcc")
-	pflag.Parse()
-	args := pflag.Args()
-
-	if len(args) < 1 {
-		fmt.Println("no input provided")
-		fmt.Println("Usage of isocc:")
-		fmt.Println("  isocc [OPTIONS] FILE")
-		fmt.Println()
-		pflag.PrintDefaults()
-		os.Exit(0)
+	var out, target string
+	var compile, assemble, verbose, keep bool
+	var args []string
+	for i := 1; i < len(os.Args); i++ {
+		arg := os.Args[i]
+		switch arg {
+		case "-o":
+			if i+1 >= len(os.Args) {
+				fatal("-o needs an argument")
+			}
+			out = os.Args[i+1]
+			i++
+		case "-c":
+			compile = true
+		case "-S":
+			assemble = true
+		case "-V":
+			verbose = true
+		case "-K":
+			keep = true
+		default:
+			switch {
+			case strings.HasSuffix(arg, ".s"), strings.HasSuffix(arg, ".S"),
+				strings.HasSuffix(arg, ".o"), strings.HasSuffix(arg, ".c"):
+				target = arg
+			default:
+				args = append(args, arg)
+			}
+		}
 	}
 
-	target := args[0]
 	base := strings.TrimSuffix(target, filepath.Ext(target))
 	targetasm := base + ".S"
 	targeto := base + ".o"
-	*includes = append(*includes, filepath.Dir(target))
+	targetdir := filepath.Dir(target)
 
 	cc := os.Getenv("ISOCC")
 	if cc == "" {
 		cc = "aarch64-none-elf-gcc"
 	}
 
-	if *verbose {
+	if verbose {
 		log.SetOutput(os.Stdout)
 		log.SetFlags(0)
 	} else {
 		log.SetOutput(io.Discard)
-	}
-
-	extra, err := shellquote.Split(*xcc)
-	if err != nil {
-		fatal(err)
 	}
 
 	// TODO: pass the flags directly and just look for files ending in .S, .o, or .c for the input (excluding ones that come after -o)
@@ -105,60 +92,43 @@ func main() {
 
 	asm := target
 	if filepath.Ext(target) != ".s" && filepath.Ext(target) != ".S" {
-		asm = temp()
+		asm = temp(targetdir)
 		stage1 := []string{
 			"-S",
 			"-o", asm,
-			"-O" + *opt,
+			target,
 		}
-		stage1 = addFlags(stage1, "-I", includes)
-		stage1 = addFlags(stage1, "-D", defines)
-		stage1 = addFlags(stage1, "-f", fflag)
-		stage1 = addFlags(stage1, "-m", mflag)
-		stage1 = addFlags(stage1, "-idirafter", idirafterflag)
-		if *debug {
-			stage1 = append(stage1, "-g")
-		}
-		stage1 = append(stage1, extra...)
-		stage1 = append(stage1, isoflags...)
 		stage1 = append(stage1, args...)
 
 		run(cc, stage1...)
 
-		if !*keep {
+		if !keep {
 			defer os.Remove(asm)
 		}
 	}
 
-	iso := temp()
-	if !*keep {
+	iso := temp(targetdir)
+	if !keep {
 		defer os.Remove(iso)
 	}
 	run("cp", asm, iso)
 	// run("isogen", asm, "-o", iso)
 
-	if !*assemble {
-		if *compile && *out == "" {
-			*out = targeto
+	if !assemble {
+		if compile && out == "" {
+			out = targeto
+		} else if !compile && out == "" {
+			out = "a.out"
 		}
 
 		stage2 := []string{
-			"-o", *out,
-			"-O" + *opt,
+			"-o", out,
 			iso,
 		}
-		if *debug {
-			stage2 = append(stage2, "-g")
-		}
-		if *compile {
+		if compile {
 			stage2 = append(stage2, "-c")
 		}
-		stage2 = addFlags(stage2, "-I", includes)
-		stage2 = addFlags(stage2, "-D", defines)
-		stage2 = addFlags(stage2, "-f", fflag)
-		stage2 = addFlags(stage2, "-m", mflag)
-		stage2 = addFlags(stage2, "-idirafter", idirafterflag)
-		stage2 = append(stage2, extra...)
+		stage2 = append(stage2, args...)
 		run(cc, stage2...)
 	} else {
 		run("cp", iso, targetasm)
