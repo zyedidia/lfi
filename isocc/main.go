531bb819113c7031fad60f62e9acfab218f27717
Zachary Yedidia <zyedidia@gmail.com>
2023-06-04 22:05:24 -0700

    Update isocc

--- isocc/main.go
+++ isocc/main.go
@@ -32,7 +32,7 @@ func run(command string, args ...string) {
 }
 
 func temp() string {
-	tmp, err := os.CreateTemp("", "isocc.*.s")
+	tmp, err := os.CreateTemp("", "isocc.*.S")
 	if err != nil {
 		fatal(err)
 	}
@@ -61,10 +61,14 @@ func main() {
 	assemble := pflag.BoolP("emit-asm", "S", false, "emit assembly code")
 	verbose := pflag.BoolP("verbose", "V", false, "verbose information")
 	keep := pflag.Bool("keep", false, "do not remove temporary files")
+	fflag := pflag.StringSliceP("f", "f", nil, "-f flags passed to gcc")
+	mflag := pflag.StringSliceP("m", "m", nil, "-m flags passed to gcc")
+	idirafterflag := pflag.StringSlice("idirafter", nil, "-idirafter flags passed to gcc")
 	pflag.Parse()
 	args := pflag.Args()
 
 	if len(args) < 1 {
+		fmt.Println("no input provided")
 		fmt.Println("Usage of isocc:")
 		fmt.Println("  isocc [OPTIONS] FILE")
 		fmt.Println()
@@ -74,8 +78,9 @@ func main() {
 
 	target := args[0]
 	base := strings.TrimSuffix(target, filepath.Ext(target))
-	targetasm := base + ".s"
+	targetasm := base + ".S"
 	targeto := base + ".o"
+	*includes = append(*includes, filepath.Dir(target))
 
 	cc := os.Getenv("ISOCC")
 	if cc == "" {
@@ -94,29 +99,46 @@ func main() {
 		fatal(err)
 	}
 
-	asm := temp()
-	stage1 := []string{
-		"-S",
-		"-o", asm,
-		"-O" + *opt,
-	}
-	stage1 = addFlags(stage1, "-I", includes)
-	stage1 = addFlags(stage1, "-D", defines)
-	if *debug {
-		stage1 = append(stage1, "-g")
-	}
-	stage1 = append(stage1, extra...)
-	stage1 = append(stage1, isoflags...)
-	stage1 = append(stage1, args...)
+	// TODO: pass the flags directly and just look for files ending in .S, .o, or .c for the input (excluding ones that come after -o)
+	// TODO: put the temp file in the same directory as the target
+	// TODO: in parser, skip lines beginning with # or \ (make them opaque or directives or something)
 
-	run(cc, stage1...)
+	asm := target
+	if filepath.Ext(target) != ".s" && filepath.Ext(target) != ".S" {
+		asm = temp()
+		stage1 := []string{
+			"-S",
+			"-o", asm,
+			"-O" + *opt,
+		}
+		stage1 = addFlags(stage1, "-I", includes)
+		stage1 = addFlags(stage1, "-D", defines)
+		stage1 = addFlags(stage1, "-f", fflag)
+		stage1 = addFlags(stage1, "-m", mflag)
+		stage1 = addFlags(stage1, "-idirafter", idirafterflag)
+		if *debug {
+			stage1 = append(stage1, "-g")
+		}
+		stage1 = append(stage1, extra...)
+		stage1 = append(stage1, isoflags...)
+		stage1 = append(stage1, args...)
+
+		run(cc, stage1...)
+
+		if !*keep {
+			defer os.Remove(asm)
+		}
+	}
+
 	iso := temp()
-
+	if !*keep {
+		defer os.Remove(iso)
+	}
 	run("cp", asm, iso)
-	run("isogen", asm, "-o", iso)
+	// run("isogen", asm, "-o", iso)
 
 	if !*assemble {
-		if *compile {
+		if *compile && *out == "" {
 			*out = targeto
 		}
 
@@ -131,14 +153,14 @@ func main() {
 		if *compile {
 			stage2 = append(stage2, "-c")
 		}
+		stage2 = addFlags(stage2, "-I", includes)
+		stage2 = addFlags(stage2, "-D", defines)
+		stage2 = addFlags(stage2, "-f", fflag)
+		stage2 = addFlags(stage2, "-m", mflag)
+		stage2 = addFlags(stage2, "-idirafter", idirafterflag)
 		stage2 = append(stage2, extra...)
 		run(cc, stage2...)
 	} else {
 		run("cp", iso, targetasm)
-	}
-
-	if !*keep {
-		os.Remove(asm)
-		os.Remove(iso)
 	}
 }
