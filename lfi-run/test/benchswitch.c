93c6630d4471e53a7aae2147b10cc746fefe87e2
Zachary Yedidia <zyedidia@gmail.com>
2023-11-20 16:52:38 -0800

    Clear integer callee-saved registers

--- lfi-run/test/benchswitch.c
+++ lfi-run/test/benchswitch.c
@@ -3,7 +3,9 @@
 static void yield_fast(int pid) {
     asm volatile (
         "ldr x30, [x21, #8];"
-        "blr x30;" ::: "x30"
+        "blr x30;" ::: "x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8",
+                       "x9", "x10", "x11", "x12", "x13", "x14", "x15", "x16",
+                       "x17", "x30"
     );
 }
 
