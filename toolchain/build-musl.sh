e51a081ca5268b0ea4ac7e5a984632f834dbfa41
Zachary Yedidia <zyedidia@gmail.com>
2023-11-28 19:16:02 -0800

    Fix mmap threshold setting

--- toolchain/build-musl.sh
+++ toolchain/build-musl.sh
@@ -14,6 +14,7 @@ CC=$PWD/../compiler/lfi-nolib-clang
 
 cp memset.S musl-$VERSION/src/string/aarch64/memset.S
 cp getopt.c musl-$VERSION/src/misc/getopt.c
+cp meta.h musl-$VERSION/src/malloc/mallocng/meta.h
 
 export CC=$CC
 export CFLAGS="--rtlib=compiler-rt -resource-dir $COMPRT -DMMAP_THRESHOLD=4294967296"
