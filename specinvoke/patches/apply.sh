42e1f7f98f58bbc1a6e4556f9c99517ec1ecd055
Zachary Yedidia <zyedidia@gmail.com>
2023-12-06 15:55:33 -0800

    Apply script

---
+++ specinvoke/patches/apply.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PATCH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/..
+
+patch -p0 < $PATCH/prm.c.patch
+patch -p0 < $PATCH/molio.c.patch
+patch -p0 < $PATCH/configfile.h.patch
+patch -p0 < $PATCH/configfile.c.patch
