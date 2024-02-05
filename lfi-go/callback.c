4d0fc8248322aea7265ef48300fb54a71bb8ed56
Zachary Yedidia <zyedidia@gmail.com>
2024-02-04 18:58:44 -0800

    Fixes for go binding

---
+++ lfi-go/callback.c
@@ -0,0 +1,7 @@
+#include <stdint.h>
+
+extern uint64_t _lfi_go_cb(void*, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
+
+uint64_t lfi_cb(void* ctxp, uint64_t num, uint64_t args[6]) {
+    _lfi_go_cb(ctxp, num, args[0], args[1], args[2], args[3], args[4], args[5]);
+}
--- lfi-go/lfi.go
+++ lfi-go/lfi.go
@@ -10,13 +10,9 @@ import (
 )
 
 /*
+#cgo LDFLAGS: -llfiverify
 #include "lfi.h"
-
-uint64_t _lfi_go_cb(void*, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
-
-uint64_t lfi_cb(void* ctxp, uint64_t num, uint64_t args[6]) {
-	_lfi_go_cb(ctxp, num, args[0], args[1], args[2], args[3], args[4], args[5]);
-}
+uint64_t lfi_cb(void* ctxp, uint64_t num, uint64_t args[6]);
 */
 import "C"
 
