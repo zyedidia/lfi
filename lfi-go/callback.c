7721ffdf0b252801735e57265c2e578c4fdf58b7
Zachary Yedidia <zyedidia@gmail.com>
2024-02-06 12:41:53 -0800

    Remove go bindings

--- lfi-go/callback.c
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-
-extern uint64_t lfi_go_cb(void*, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
-
-uint64_t lfi_cb(void* ctxp, uint64_t num, uint64_t args[6]) {
-    return lfi_go_cb(ctxp, num, args[0], args[1], args[2], args[3], args[4], args[5]);
-}
