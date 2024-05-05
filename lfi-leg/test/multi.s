e519ac5f72e8e375a7b45b59185b88876dc82202
Zachary Yedidia <zyedidia@gmail.com>
2024-05-05 16:44:22 -0700

    Some tests for lfi-leg

--- lfi-leg/test/multi.s
+++ lfi-leg/test/multi.s
@@ -1,1 +1,14 @@
-ldp x0, x1, [x2]
+ldp x9, x10, [x2]
+
+ldrh w12, [x16, w12, uxtw]
+
+ld1 { v16.s }[1], [x11]
+
+eor x1, x1, x3, ror #(64 - 23)
+subs x1, x1, 64 - 23
+
+ldr x1, [x1, #4188]
+
+fmov d10, 2.5e1-1
+fmov d10, 2.5e1+1
+ldr x1, [x1, #:lo12:map]
