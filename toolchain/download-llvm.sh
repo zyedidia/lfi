dc8089775f158150ea7d675a94e6f4e479011735
Zachary Yedidia <zyedidia@gmail.com>
2024-05-09 06:54:32 +0000

    Update toolchain and readme

--- toolchain/download-llvm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# usage ./download-llvm.sh VERSION
-
-VERSION=$1
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-$VERSION/llvm-project-$VERSION.src.tar.xz
-tar -xf llvm-project-$VERSION.src.tar.xz
--- toolchain/download-musl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-VERSION=$1
-wget https://musl.libc.org/releases/musl-$VERSION.tar.gz
-tar -xf musl-$VERSION.tar.gz
