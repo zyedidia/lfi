6e3f2709c022d05dc823deb4d44df6e546e545d4
Zachary Yedidia <zyedidia@gmail.com>
2024-05-09 23:03:04 +0000

    Update installation script

--- install.sh
+++ install.sh
@@ -2,15 +2,20 @@
 
 set -ex
 
+git submodule update --init
+
 PREFIX=$1
 
 mkdir -p $PREFIX
 mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/toolchain
+mkdir -p $PREFIX/clang
+mkdir -p $PREFIX/gcc
 
-go build -o $PREFIX/bin ./lfi-gen
+go build -o $PREFIX/bin ./lfi-as
 go build -o $PREFIX/bin ./lfi-compile
 go build -o $PREFIX/bin ./specinvoke
+
+(cd lfi-leg && make lfi-leg-arm64 && mv lfi-leg-arm64 $PREFIX/bin)
 
 export PATH=$PREFIX/bin:$PATH
 
@@ -18,4 +23,8 @@ export PATH=$PREFIX/bin:$PATH
 
 (cd lfi-run && knit && mv lfi-run $PREFIX/bin)
 
-(cd toolchain && ./install-toolchain.sh $PREFIX/toolchain)
+# install clang
+(cd toolchain/lfi-clang && ./install-toolchain.sh $PREFIX/clang)
+
+# install gcc
+(cd toolchain/lfi-gcc && ./install-toolchain.sh $PREFIX/gcc)
