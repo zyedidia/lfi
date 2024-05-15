21d1782dc8c4d31cfd66c987506c29b548aeb0f0
Zachary Yedidia <zyedidia@gmail.com>
2024-05-15 06:02:49 +0000

    Implement poc for x30

--- lfi-leg/test/arm64/poc.s
+++ lfi-leg/test/arm64/poc.s
@@ -1,4 +1,19 @@
 adr x0, hello
 >>>
-adr x22, hello
-mov w0, w22
+adr x18, hello
+mov w0, w18
+------
+str x30, [x0]
+>>>
+mov w22, w30
+str x22, [x21, w0, uxtw]
+------
+str x30, [sp]
+>>>
+mov w22, w30
+str x22, [sp]
+------
+stp x29, x30, [sp]
+>>>
+mov w22, w30
+stp x29, x22, [sp]
