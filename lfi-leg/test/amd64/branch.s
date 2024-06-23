125a368562fbba567da77529949c7b9c05d1399e
Zachary Yedidia <zyedidia@gmail.com>
2024-06-22 17:02:47 -0700

    Back to 16-byte bundles

--- lfi-leg/test/amd64/branch.s
+++ lfi-leg/test/amd64/branch.s
@@ -2,21 +2,23 @@ call *%rax
 >>>
 .bundle_align_mode 4
 movq %rax, %r15
-.p2align 4
+.bundle_lock
 andl $0xfffffff0, %r15d
 orq %r14, %r15
 .byte 0x66, 0x0f, 0x1f, 0x44, 0x00, 0x00
 callq *%r15
+.bundle_unlock
 ------
 call *32+output(%rip)
 >>>
 .bundle_align_mode 4
 movq 32+output(%rip), %r15
-.p2align 4
+.bundle_lock
 andl $0xfffffff0, %r15d
 orq %r14, %r15
 .byte 0x66, 0x0f, 0x1f, 0x44, 0x00, 0x00
 callq *%r15
+.bundle_unlock
 ------
 jmp *(%rax)
 >>>
@@ -32,11 +34,12 @@ callq *(%rax, %rdi, 4)
 >>>
 .bundle_align_mode 4
 movq %gs:(%eax, %edi, 4), %r15
-.p2align 4
+.bundle_lock
 andl $0xfffffff0, %r15d
 orq %r14, %r15
 .byte 0x66, 0x0f, 0x1f, 0x44, 0x00, 0x00
 callq *%r15
+.bundle_unlock
 ------
 notrack jmp *%rax
 >>>
@@ -51,8 +54,16 @@ callq *__cxa_finalize@GOTPCREL(%rip)
 >>>
 .bundle_align_mode 4
 movq __cxa_finalize@GOTPCREL(%rip), %r15
-.p2align 4
+.bundle_lock
 andl $0xfffffff0, %r15d
 orq %r14, %r15
 .byte 0x66, 0x0f, 0x1f, 0x44, 0x00, 0x00
 callq *%r15
+.bundle_unlock
+------
+callq foo
+>>>
+.bundle_align_mode 4
+.p2align 4
+.byte 0x66, 0x66, 0x2e, 0x0f, 0x1f, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00
+callq foo
