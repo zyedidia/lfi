7587a1af9e82f6f63a4f2e3ffd0f002b76f3f10b
Yavor <yavor.litchev@innoiso.com>
2024-06-28 17:26:54 -0400

    Updated bdd compression

--- lfi-veribdd/generator/bdd.go
+++ lfi-veribdd/generator/bdd.go
@@ -18,6 +18,15 @@ type Node struct {
 	hi uint16
 }
 
+type Node2 struct {
+	shift uint8 // low 5 bits represent the ammount of location shifted (equivalent to v value in original Node struct), 
+	// high 3 bits represent the size of the mask to be considered (0 -> size 1, 1 -> size 2, ... 7 -> size 8)
+	v uint8 // value to be checked
+	lo uint16 // goes to if mask check rejects
+	hi uint16 // goes to if mask check accepts
+	parents []int // list of parent nodes via index value
+}
+
 func toInt(s string) int {
 	i, err := strconv.Atoi(s)
 	if err != nil {
@@ -55,6 +64,29 @@ func writeC(nodes []Node, w io.Writer){
 		}
 		fmt.Fprintf(w, "node%d:\n", i)
 		fmt.Fprintf(w, "\tif((input>>%d) & 0x1)\n", n.v)
+		fmt.Fprintf(w, "\t\tgoto node%d;\n", n.hi)
+		fmt.Fprintf(w, "\telse \n\t\tgoto node%d;\n", n.lo)
+	}
+	fmt.Fprintln(w, "}")
+}
+
+
+func writeC2(nodes []Node2, w io.Writer){
+	fmt.Fprintln(w,"#include <stdint.h>")
+	fmt.Fprintln(w,"#include <stdbool.h>")
+	fmt.Fprintln(w,"bool evaluate(uint32_t input){")
+	fmt.Fprintf(w, "\tgoto node%d;\n", len(nodes) - 1)
+	fmt.Fprintf(w, "node0: return false;\n")
+	fmt.Fprintf(w, "node1: return true;\n")
+	for i, n := range nodes {
+		if i < 2  {
+			continue
+		}
+		
+		var mask uint32 = (1 << ((n.shift >> 5) + (n.shift & 0x1F) + 1)) - (1 << (n.shift & 0x1F))
+		var val uint32 = uint32(n.v) << (n.shift & 0x1F)
+		fmt.Fprintf(w, "node%d:\n", i)
+		fmt.Fprintf(w, "\tif((input & %d) == %d)\n", mask, val)
 		fmt.Fprintf(w, "\t\tgoto node%d;\n", n.hi)
 		fmt.Fprintf(w, "\telse \n\t\tgoto node%d;\n", n.lo)
 	}
@@ -109,6 +141,8 @@ func main() {
 		v := toInt(parts[1])
 		lo := toInt(parts[2])
 		hi := toInt(parts[3])
+		
+		// fmt.Fprintf(os.Stdout, "\tgoto node%d;\n", uint16(nodeId[lo]))
 
 		nodes = append(nodes, Node{
 			v:  uint8(v),
@@ -117,12 +151,207 @@ func main() {
 		})
 		nodeId[id] = len(nodes) - 1
 	}
+	
+	nodes2 := make([]Node2, 0, n)
+	for i, N := range nodes[:] {
+		// fmt.Fprintf(os.Stdout, "\tgoto node%d;\n", N.hi)
+		nodes2 = append(nodes2, Node2{
+			shift: N.v,
+			v: 1,
+			lo: N.lo,
+			hi: N.hi,
+			parents: make([]int, 0, n),
+		})
+		
+		nodes2[N.lo].parents = append(nodes2[N.lo].parents, i)
+		nodes2[N.hi].parents = append(nodes2[N.hi].parents, i)
+		
+		// fmt.Println(nodes2[N.lo].parents)
+	}
+	
+	
+	/* for i, n := range nodes2 {
+	
+	} */
+	
+	for i := len(nodes2) - 1; i >= 2; i-- {
+		origN := &nodes2[i]
+		curN := nodes2[i]
+		curNNumerical := uint16(i)
+		
+		if origN.lo < 2 && origN.hi < 2 { // If both children lead to end of BDD, cannot compress further, so exit case
+			continue
+		}
+		
+		// if origN.shift - nodes2
+		
+		var checkStr uint8 = 0
+		var bitI uint8 = 0
+		var increasing bool = false // = origN.shift <  // Is the bit shift value increasing as one descends upon the tree
+		var commonNode uint16
+		
+		var fixed bool = false
+		
+		// if i == 3 {fmt.Printf("%d == %d\n", origN.hi, nodes2[origN.lo].hi)}
+		
+		if origN.lo == nodes2[origN.hi].lo {
+			commonNode = origN.lo
+			curN = nodes2[origN.hi]
+			curNNumerical = origN.hi
+			
+			abs := int(origN.shift) - int(curN.shift)
+			
+			if(abs < 0){ abs = -abs; increasing = true}
+			
+			
+			if len(curN.parents) > 1 || abs > 1 {continue} // If the current node has multiple references to it, it cannot be a node that is compressed and ignored
+			// also if the shift distance is greater than 1
+			
+			checkStr = 0b11
+			bitI += 2
+			fixed = true
+			
+		} else if origN.lo == nodes2[origN.hi].hi {
+			commonNode = origN.lo
+			curN = nodes2[origN.hi]
+			curNNumerical = origN.hi
+			
+			abs := int(origN.shift) - int(curN.shift)
+			
+			if(abs < 0){ abs = -abs; increasing = true}
+			
+			
+			if len(curN.parents) > 1 || abs > 1 {continue} // If the current node has multiple references to it, it cannot be a node that is compressed and ignored
+			// also if the shift distance is greater than 1
+			
+			checkStr = 0b10
+			if increasing{checkStr = 0b01}
+			bitI += 2
+			fixed = true
+			
+		} else if origN.hi == nodes2[origN.lo].lo {
+			commonNode = origN.hi
+			curN = nodes2[origN.lo]
+			curNNumerical = origN.lo
+			
+			abs := int(origN.shift) - int(curN.shift)
+			
+			if(abs < 0){ abs = -abs; increasing = true}
+			
+			
+			if len(curN.parents) > 1 || abs > 1 {continue} // If the current node has multiple references to it, it cannot be a node that is compressed and ignored
+			// also if the shift distance is greater than 1
+			
+			checkStr = 0b01
+			if increasing{checkStr = 0b10}
+			bitI += 2
+			fixed = true
+			
+		} else if origN.hi == nodes2[origN.lo].hi {
+		
+
+			commonNode = origN.hi
+			curN = nodes2[origN.lo]
+			curNNumerical = origN.lo
+			
+			abs := int(origN.shift) - int(curN.shift)
+			
+			if(abs < 0){ abs = -abs; increasing = true}
+			// if i == 3 {fmt.Println(abs)}
+			
+			
+			if len(curN.parents) > 1 || abs > 1 {continue} // If the current node has multiple references to it, it cannot be a node that is compressed and ignored
+			// also if the shift distance is greater than 1
+			
+			// if i == 3 {fmt.Println(curN.parents)}
+			checkStr = 0b00
+			bitI += 2
+			fixed = true
+			
+		} 
+		
+		
+		if fixed {
+		
+			// fmt.Println("fixed")
+			for bitI < 8 {
+				// if len(nodes2[curN.].parents) == 1 && nodes2[]
+				
+				var alternateNode uint16 // Set this variable to the node other than the common node if it exists
+				if curN.lo == commonNode {
+					alternateNode = curN.hi
+				} else if curN.hi == commonNode {
+					alternateNode = curN.lo
+				} else {
+					break
+				}
+				
+				
+				// If the alternate node is well formed for continuing the chain (no parents, continuing to increase/decrease), continue the chain
+				abs := int(curN.shift) - int(nodes2[alternateNode].shift)
+			
+				if(increasing){ abs = -abs}
+			
+			
+				if len(nodes2[alternateNode].parents) > 1 || abs != 1 {break}
+				
+				
+				// Conditions passed, update the check string and progress down the chain
+				var newBit uint8 = 0 
+				if alternateNode == curN.hi {newBit = 1}
+				
+				if increasing {
+					checkStr |= newBit << bitI
+				} else {
+					checkStr <<= 1
+					checkStr |= newBit
+				}
+				
+				
+				curN = nodes2[alternateNode]
+				curNNumerical = alternateNode
+				bitI++
+				
+				
+			}
+			
+			// Update the node to the actual value
+			
+			origN.v = checkStr
+			
+			// Update the shift to be the lower of the two shifts, as that is where the bits will be checked
+			// var uShift uint8 = nodes2[nodes2[curN.lo].parents[0]].shift
+			var uShift uint8 = nodes2[curN.parents[0]].shift
+			if uShift < origN.shift {origN.shift = uShift} 
+			
+			origN.shift |= (bitI - 1) << 5 // Add the length of the bit string to the top unused 3 bits of the shift for saving
+			
+			var alternateNode uint16 // Set this variable to the node other than the common node if it exists
+			if nodes2[curNNumerical].lo == commonNode {
+				alternateNode = curN.hi
+			} else if curN.hi == commonNode {
+				alternateNode = curN.lo
+			}
+			
+			origN.hi = alternateNode // curNNumerical // uint16(nodes2[curN.lo].parents[0])
+			origN.lo = commonNode
+			
+			// fmt.Printf("node%d: %d\n", i, origN.v)
+			// fmt.Printf("%d\n", nodes2[i].v)
+			
+		}
+		
+	}
+	
+	
+	
 
 	if *dot {
 		writeDot(nodes, os.Stdout)
 	} else if *binary {
 		writeBinary(nodes, os.Stdout)
 	} else {
-		writeC(nodes, os.Stdout)
+		// writeC(nodes, os.Stdout)
+		writeC2(nodes2, os.Stdout)
 	}
 }
