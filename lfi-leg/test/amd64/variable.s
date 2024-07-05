2a7f7365673938a31fe469554c07b6d30c278dd1
Zachary Yedidia <zyedidia@gmail.com>
2024-07-05 15:08:52 -0700

    Variable-sized sandboxes for x86 using shlx/shrx

--- lfi-leg/test/amd64/variable.s
+++ lfi-leg/test/amd64/variable.s
@@ -1,9 +1,9 @@
 mov (%rsi), %rdx
 >>>
 .bundle_align_mode 4
-movq %rsi, %r15
 .bundle_lock
-andq %r13, %r15
+shlx %r13, %rsi, %r15
+shrx %r13, %r15, %r15
 mov (%r14, %r15), %rdx
 .bundle_unlock
 ------
@@ -13,7 +13,8 @@ callq *(%rsp)
 movq (%rsp), %r15
 .p2align 4
 .bundle_lock
-andq %r13, %r15
+shlx %r13, %r15, %r15
+shrx %r13, %r15, %r15
 orq %r14, %r15
 .byte 0x66, 0x0f, 0x1f, 0x44, 0x00, 0x00
 callq *%r15
@@ -24,9 +25,9 @@ imulq $24, -88(%rdi), %rax
 >>>
 .bundle_align_mode 4
 imulq $24, -88(%rsp), %rax
-movq %rdi, %r15
 .bundle_lock
-andq %r13, %r15
+shlx %r13, %rdi, %r15
+shrx %r13, %r15, %r15
 imulq $24, -88(%r14, %r15), %rax
 .bundle_unlock
 ------
@@ -34,9 +35,9 @@ movq %dh, (%rax)
 >>>
 .bundle_align_mode 4
 movq %rdi, %r15
-movq %rax, %rdi
 .bundle_lock
-andq %r13, %rdi
+shlx %r13, %rax, %rdi
+shrx %r13, %rdi, %rdi
 leaq (%r14, %rdi), %rdi
 movq %dh, (%rdi)
 .bundle_unlock
@@ -46,7 +47,8 @@ rep stosq
 >>>
 .bundle_align_mode 4
 .bundle_lock
-andq %r13, %rdi
+shlx %r13, %rdi, %rdi
+shrx %r13, %rdi, %rdi
 leaq (%r14, %rdi), %rdi
 rep stosq
 .bundle_unlock
