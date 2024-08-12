0508e17dc2e3af63c8f7b298d2a47d529d24889f
Zachary Yedidia <zyedidia@gmail.com>
2024-08-12 13:09:10 -0700

    Simplify genbddsingle

--- lfi-veribdd/generator/genbddsingle.cc
+++ lfi-veribdd/generator/genbddsingle.cc
@@ -2,16 +2,8 @@
 #include <stdint.h>
 #include <sys/mman.h>
 #include <fcntl.h>
-#include <thread>
-#include <mutex>
 
 #include "lfi.h"
-
-#define NTHREADS 8
-const size_t n_verify = 0x0fffffffULL + 1;
-
-
-std::mutex bdd_mutex;
 
 bdd
 add_value(uint32_t val)
@@ -19,30 +11,13 @@ add_value(uint32_t val)
     bdd total = bddtrue;
     for (size_t i = 0; i < 32; i++) {
         uint32_t bit = (val >> i) & 1;
-        bdd_mutex.lock();
         bdd var = bdd_ithvar(i);
         if (!bit) {
             var = bdd_not(var);
         }
-        bdd_mutex.unlock();
         total &= var;
     }
     return total;
-}
-
-
-void
-add_value_range(uint32_t start, uint32_t end, bdd &result)
-{
-    for (size_t i = start; i < end; i++) {
-        if (i % 5000000 == 0) {
-            std::cerr << "Thread ID: " << std::this_thread::get_id() << " - ";
-            fprintf(stderr, "%.1f\n", (float) i / (float) n_verify * 100);
-        }
-        if (lfi_verify_insn(i)) {
-            result |= add_value(i);
-        }
-    }
 }
 
 int
@@ -53,7 +28,7 @@ main(int argc, char* argv[])
         return 1;
     }
 
-    // size_t n_verify = 0x0ffffffffULL + 1;
+    size_t n_verify = 0x0ffffffffULL + 1;
 
     bdd_init(10000000, 10000);
     bdd_setvarnum(32);
@@ -67,26 +42,15 @@ main(int argc, char* argv[])
     };
     bdd_setvarorder(order);
 
-    // bdd full = bddfalse;
-    std::thread threads[NTHREADS];
-    bdd results[NTHREADS];
-
-    for (int i = 0; i < NTHREADS; i++) 
-        results[i] = bddfalse;
-
-
-    for (int i = 0; i < NTHREADS; i++) {
-        threads[i] = std::thread(add_value_range, (uint32_t) (n_verify*i)/NTHREADS, (uint32_t) (n_verify*(i+1))/NTHREADS, std::ref(results[i]));
-    }
-
-    for (int i = 0; i < NTHREADS; i++) 
-        threads[i].join();
-    fprintf(stderr, "Finished all threads\n");
-
     bdd full = bddfalse;
-    for (int i = 0; i < NTHREADS; i++) 
-        full |= results[i];
-
+    for (size_t i = 0; i < n_verify; i++) {
+        if (i % 5000000 == 0) {
+            fprintf(stderr, "%.1f\n", (float) i / (float) n_verify * 100);
+        }
+        if (lfi_verify_insn((uint32_t) i)) {
+            full |= add_value(i);
+        }
+    }
 
     FILE* f = fopen(argv[1], "w");
     if (!f) {
