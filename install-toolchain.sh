015cea87ae01a792dbbee1780f18c5aa6328835e
Zachary Yedidia <zyedidia@gmail.com>
2024-08-13 14:40:00 -0700

    Update installation scripts

--- install-toolchain.sh
+++ install-toolchain.sh
@@ -9,26 +9,16 @@ git submodule update --init
 PREFIX=$1
 
 mkdir -p $PREFIX
-mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/clang
 mkdir -p $PREFIX/gcc
-
-go build -o $PREFIX/bin ./lfi-as
-go build -o $PREFIX/bin ./lfi-compile
-go build -o $PREFIX/bin ./specinvoke
 
 cp LICENSE $PREFIX
 cp README.md $PREFIX
 
-(cd lfi-leg && make lfi-leg-arm64 && mv lfi-leg-arm64 $PREFIX/bin)
+meson build --prefix $PREFIX
+ninja -C build install
 
 export PATH=$PREFIX/bin:$PATH
-
-(cd lfi-verify && cargo build --release && mv ./target/release/lfi-verify $PREFIX/bin/lfi-verify-verbose)
-
-(cd lfi-veribdd && knit && mv lfi-verify $PREFIX/bin)
-
-(cd lfi-run && knit && mv lfi-run $PREFIX/bin)
 
 # install clang
 (cd toolchain/lfi-clang && ./install-toolchain.sh $PREFIX/clang)
