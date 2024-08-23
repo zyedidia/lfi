6bb3d17229d00bb0cedc3a63d57c262e2d3003e6
Zachary Yedidia <zyedidia@gmail.com>
2024-08-23 15:38:48 -0700

    Remove garbage collection output from bdd benchmarking script

--- lfi-veribdd/generator/test.sh
+++ lfi-veribdd/generator/test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 for i in {1..16}; do
-    ../../build/lfi-veribdd/generator/genbdd out.bdd "$i" # 2>/dev/null
-	echo "--------------------------------------------------------------"
+    ../../build/lfi-veribdd/generator/genbdd out.bdd "$i" 2>/dev/null | sed '/Garbage collection/d'
 done
