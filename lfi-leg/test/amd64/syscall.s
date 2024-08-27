0ab8c5a02379ae7221fccc0ba6d4f336e2ef447a
Zachary Yedidia <zyedidia@gmail.com>
2024-08-27 01:02:16 -0700

    Fix tls and syscalls labels

--- lfi-leg/test/amd64/syscall.s
+++ lfi-leg/test/amd64/syscall.s
@@ -2,7 +2,7 @@ syscall
 >>>
 .bundle_align_mode 4
 .bundle_lock
-leaq 1f(%rip), %r11
+leaq .LFI_SYS0(%rip), %r11
 jmpq *(%r14)
-1:
+.LFI_SYS0:
 .bundle_unlock
--- lfi-leg/test/amd64/tls.s
+++ lfi-leg/test/amd64/tls.s
@@ -3,9 +3,9 @@ mov %fs:0, %rdx
 .bundle_align_mode 4
 pushq %rax
 .bundle_lock
-leaq 1f(%rip), %r11
+leaq .LFI_TLS0(%rip), %r11
 jmpq *8(%r14)
-1:
+.LFI_TLS0:
 .bundle_unlock
 movq %rax, %rdx
 popq %rax
@@ -14,18 +14,18 @@ mov %fs:0, %rax
 >>>
 .bundle_align_mode 4
 .bundle_lock
-leaq 1f(%rip), %r11
+leaq .LFI_TLS0(%rip), %r11
 jmpq *8(%r14)
-1:
+.LFI_TLS0:
 .bundle_unlock
 ------
 mov %fs:0x0, %rax
 >>>
 .bundle_align_mode 4
 .bundle_lock
-leaq 1f(%rip), %r11
+leaq .LFI_TLS0(%rip), %r11
 jmpq *8(%r14)
-1:
+.LFI_TLS0:
 .bundle_unlock
 ------
 callq *x@TLSCALL(%rax)
