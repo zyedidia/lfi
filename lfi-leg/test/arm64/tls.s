0634e20bd88f5424c54c4cddd5b01d6327f41b09
Zachary Yedidia <zyedidia@gmail.com>
2024-09-24 22:02:38 +0000

    Fix tls test

--- lfi-leg/test/arm64/tls.s
+++ lfi-leg/test/arm64/tls.s
@@ -1,24 +1,26 @@
 mrs x1, tpidr_el0
 >>>
-stp x0, x30, [sp, #-16]!
+str x0, [sp, #-16]!
+mov w22, w30
 ldr x30, [x21, #8]
 blr x30
 mov x1, x0
-ldp x0, x22, [sp], 16
+ldr x0, [sp], 16
 add x30, x21, w22, uxtw
 ------
 mrs x0, tpidr_el0
 >>>
-mov x22, x30
+mov w22, w30
 ldr x30, [x21, #8]
 blr x30
 add x30, x21, w22, uxtw
 ------
 msr tpidr_el0, x1
 >>>
-stp x0, x30, [sp, #-16]!
+str x0, [sp, #-16]!
+mov w22, w30
 mov x0, x1
 ldr x30, [x21, #16]
 blr x30
-ldp x0, x22, [sp], 16
+ldr x0, [sp], 16
 add x30, x21, w22, uxtw
