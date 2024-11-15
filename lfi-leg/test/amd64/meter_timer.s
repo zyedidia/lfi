bb52e4c117c69bf2f726d10bb4d53e9d4533f022
Zachary Yedidia <zyedidia@gmail.com>
2024-11-15 15:18:12 -0800

    Fix call instruction metering

--- lfi-leg/test/amd64/meter_timer.s
+++ lfi-leg/test/amd64/meter_timer.s
@@ -23,3 +23,12 @@ orq %r14, %rax
 leaq 0(%r12), %r12
 jmpq *%rax
 .bundle_unlock
+------
+call foo
+>>>
+.bundle_align_mode 4
+.bundle_lock
+leaq 0(%r12), %r12
+callq foo
+.bundle_unlock
+.p2align 4
