3c4db364208c790ff0be8229fa9b1a28e4ffd368
Zachary Yedidia <zyedidia@gmail.com>
2025-02-23 03:08:10 -0800

    Fix tuxerr

--- liblfi/host/error.h
+++ liblfi/host/error.h
@@ -5,12 +5,10 @@
 static inline int
 tuxerr(int err)
 {
-    if (err >= 0)
-        return err;
     switch (err) {
-    case -ENOSYS:
+    case ENOSYS:
         return -TUX_ENOSYS;
-    case -EINVAL:
+    case EINVAL:
         return -TUX_EINVAL;
     default:
         return -TUX_EINVAL;
