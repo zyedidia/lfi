20389fb6f932bd4e84ceac70db0190c77e660d87
Zachary Yedidia <zyedidia@gmail.com>
2025-03-19 14:42:51 -0700

    Fix stores-only rewriting for cmp and xchg

--- lfi-leg/test/amd64/storesonly.s
+++ lfi-leg/test/amd64/storesonly.s
@@ -40,3 +40,18 @@ andl $0xffffffe0, %r11d
 orq %r14, %r11
 jmpq *%r11
 .bundle_unlock
+------
+xchg (%rax), %rdi
+>>>
+.bundle_align_mode 5
+xchg %gs:(%eax), %rdi
+------
+cmpb $0x0, (%rax)
+>>>
+.bundle_align_mode 5
+cmpb $0x0, (%rax)
+------
+cmpb (%rax), $0x0
+>>>
+.bundle_align_mode 5
+cmpb (%rax), $0x0
